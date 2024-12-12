4c4b69c908a1153bb09e421482f8588e73c46bc5
celaireo <celaireo@gmail.com>
2024-12-12 12:28:40 +0100

    ajout du fichiers json, Setting fragments et preferences.xml

---
+++ app/src/main/java/com/example/android_roomdao/ui/theme/settings/SettingsFragment.kt
@@ -0,0 +1,10 @@
+/*
+package com.example.android_roomdao.ui.theme.settings
+
+import androidx.preference.PreferenceFragmentCompat
+import android.os.Bundle
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        //setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+}*/
